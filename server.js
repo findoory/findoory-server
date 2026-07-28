7dce12e95e6d99dac77c5a671157f40bf1b86761
findoory <findoory@gmail.com>
2026-07-28 16:44:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,28 +4,48 @@ const app = express();
 
 app.use(express.json());
 
+
 app.post("/", async (req, res) => {
 
   const response = await fetch(
-    "https://script.google.com/macros/s/AKfycby-UjWELO1GhIUPd0RvrxG7DAfUCjkTtA0RDObRtzQp0n6B4Y1P1X43lOxD5Cvha4FR/exec"
+    "https://script.google.com/macros/s/AKfycbzF7AlsflryXwcEtkM77vyuaQ5rkhfmr-jJFr79It7AdDfeHmQkoOEqoLxuC0A4KkQi/exec"
   );
 
   const data = await response.json();
 
+  const outputs = [];
+
+  data.story.forEach(item => {
+
+    if (item.type === "bot") {
+      outputs.push({
+        simpleText: {
+          text: item.content
+        }
+      });
+    }
+
+    if (item.type === "image") {
+      outputs.push({
+        simpleImage: {
+          imageUrl: item.asset,
+          altText: "핀도리 이미지"
+        }
+      });
+    }
+
+  });
+
+
   res.json({
     version: "2.0",
     template: {
-      outputs: [
-        {
-          simpleText: {
-            text: data.story[1].content
-          }
-        }
-      ]
+      outputs: outputs
     }
   });
 
 });
+
 
 app.listen(3000, () => {
   console.log("Findoory server running");
